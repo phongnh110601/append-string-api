0f7738bd78068c40eced5d0e581bd71e09e3e97d
nhphong <nhphong@vnpt.vn>
2026-09-10 11:45:35 +0700

    refactor port

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.get('/read', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+// THÊM THAM SỐ '0.0.0.0' VÀO ĐÂY
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
